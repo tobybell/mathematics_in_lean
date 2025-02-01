adf0691415bf5eb3e4d23951242a5defab25e205
Toby Bell <tbell@stanford.edu>
2025-02-01 14:54:39 -0800

    Finish exercises for 2.3

--- MIL/C02_Basics/S03_Using_Theorems_and_Lemmas.lean
+++ MIL/C02_Basics/S03_Using_Theorems_and_Lemmas.lean
@@ -44,7 +44,10 @@ example (x : ℝ) : x ≤ x :=
 
 -- Try this.
 example (h₀ : a ≤ b) (h₁ : b < c) (h₂ : c ≤ d) (h₃ : d < e) : a < e := by
-  sorry
+  apply lt_of_le_of_lt h₀
+  apply lt_trans h₁
+  apply lt_of_le_of_lt h₂
+  exact h₃
 
 example (h₀ : a ≤ b) (h₁ : b < c) (h₂ : c ≤ d) (h₃ : d < e) : a < e := by
   linarith
@@ -86,21 +89,28 @@ example (h₀ : a ≤ b) (h₁ : c < d) : a + exp c + e < b + exp d + e := by
     apply exp_lt_exp.mpr h₁
   apply le_refl
 
-example (h₀ : d ≤ e) : c + exp (a + d) ≤ c + exp (a + e) := by sorry
+example (h₀ : d ≤ e) : c + exp (a + d) ≤ c + exp (a + e) := by
+  apply add_le_add_left
+  apply exp_le_exp.mpr
+  apply add_le_add_left h₀
 
 example : (0 : ℝ) < 1 := by norm_num
 
 example (h : a ≤ b) : log (1 + exp a) ≤ log (1 + exp b) := by
-  have h₀ : 0 < 1 + exp a := by sorry
+  have h₀ : 0 < 1 + exp a := by
+    apply add_pos
+    · norm_num
+    · apply exp_pos
   apply log_le_log h₀
-  sorry
+  apply add_le_add_left
+  apply exp_le_exp.mpr h
 
 example : 0 ≤ a ^ 2 := by
-  -- apply?
   exact sq_nonneg a
 
 example (h : a ≤ b) : c - exp b ≤ c - exp a := by
-  sorry
+  apply sub_le_sub_left
+  apply exp_le_exp.mpr h
 
 example : 2*a*b ≤ a^2 + b^2 := by
   have h : 0 ≤ a^2 - 2*a*b + b^2
@@ -113,15 +123,35 @@ example : 2*a*b ≤ a^2 + b^2 := by
     _ ≤ 2*a*b + (a^2 - 2*a*b + b^2) := add_le_add (le_refl _) h
     _ = a^2 + b^2 := by ring
 
-example : 2*a*b ≤ a^2 + b^2 := by
+theorem thing : 2*a*b ≤ a^2 + b^2 := by
   have h : 0 ≤ a^2 - 2*a*b + b^2
   calc
     a^2 - 2*a*b + b^2 = (a - b)^2 := by ring
     _ ≥ 0 := by apply pow_two_nonneg
   linarith
 
+theorem thing2 : -(2*a*b) ≤ a^2 + b^2 := by
+  have h : 0 ≤ a^2 + 2*a*b + b^2
+  calc
+    a^2 + 2*a*b + b^2 = (a + b)^2 := by ring
+    _ ≥ 0 := by apply pow_two_nonneg
+  linarith
+
 example : |a*b| ≤ (a^2 + b^2)/2 := by
-  sorry
+  have h : (0 : ℝ) < 2
+  norm_num
+
+  have h2 : a * b = 2 * a * b / 2
+  rw [mul_assoc 2, mul_comm 2]
+  norm_num
+
+  apply abs_le'.mpr
+  constructor
+  · rw [h2]
+    apply (div_le_div_right h).mpr
+    apply thing
+  · rw [h2, ← neg_div]
+    apply (div_le_div_right h).mpr
+    apply thing2
 
 #check abs_le'.mpr
-
