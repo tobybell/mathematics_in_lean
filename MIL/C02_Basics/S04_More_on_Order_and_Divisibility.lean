a3d5c24ef1649f229017345135f7668bc5cbbff9
Toby Bell <tbell@stanford.edu>
2025-02-03 16:16:17 -0800

    Finish exercises for 2.4

--- MIL/C02_Basics/S04_More_on_Order_and_Divisibility.lean
+++ MIL/C02_Basics/S04_More_on_Order_and_Divisibility.lean
@@ -83,8 +83,12 @@ example : min a b + c = min (a + c) (b + c) := by
 
 #check (abs_add : ∀ a b : ℝ, |a + b| ≤ |a| + |b|)
 
-example : |a| - |b| ≤ |a - b| :=
-  sorry
+#check add_sub_cancel_right
+
+example : |a| - |b| ≤ |a - b| := by
+  let h := abs_add (a - b) b
+  nth_rw 1 [← sub_add_cancel a b]
+  linarith
 end
 
 section
@@ -101,7 +105,13 @@ example : x ∣ x ^ 2 := by
   apply dvd_mul_left
 
 example (h : x ∣ w) : x ∣ y * (x * z) + x ^ 2 + w ^ 2 := by
-  sorry
+  apply dvd_add
+  apply dvd_add
+  apply dvd_mul_of_dvd_right
+  apply dvd_mul_right
+  apply dvd_mul_left
+  apply dvd_mul_of_dvd_right
+  exact h
 end
 
 section
@@ -112,6 +122,4 @@ variable (m n : ℕ)
 #check (Nat.lcm_zero_right n : Nat.lcm n 0 = 0)
 #check (Nat.lcm_zero_left n : Nat.lcm 0 n = 0)
 
-example : Nat.gcd m n = Nat.gcd n m := by
-  sorry
-end
+example : Nat.gcd m n = Nat.gcd n m := Nat.gcd_comm m n
